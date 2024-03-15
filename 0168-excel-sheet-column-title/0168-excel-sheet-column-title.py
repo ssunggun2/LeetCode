22f7bc1bd5967a451e2c8ea5f297f162234cb14e
Sung Geun An <85274662+ssunggun2@users.noreply.github.com>
2024-03-15 23:40:26 +0900

    Time: 34 ms (60.41%), Space: 16.5 MB (95.41%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.py
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.py
@@ -1,8 +1,12 @@
 class Solution:
     def convertToTitle(self, columnNumber: int) -> str:
+        hash_map = {1 : "A", 2 : "B", 3 : "C", 4 : "D", 5 : "E" , 6 : "F", 7 : "G" , 8 : "H", 9 : "I", 10 : "J", 11 : "K", 12 : "L", 13 : "M", 
+                    14 : "N", 15 : "O", 16 : "P", 17 : "Q", 18 : "R", 19 : "S", 20 : "T", 21 : "U", 22 : "V", 23 : "W" , 24 : "X", 25 : "Y", 26 : "Z"}
         r = []
+        if columnNumber // 27 < 1:
+            return hash_map[columnNumber]
         while columnNumber:
             columnNumber, mod = divmod(columnNumber - 1, 26)
-            r.append(chr(65+mod))
+            r.append(hash_map[mod+1])
             
         return ''.join(reversed(r))
