91d6a4d24b17010cdb6b05f8dc49a6eab582ed32
Sung Geun An <85274662+ssunggun2@users.noreply.github.com>
2024-07-01 18:08:53 +0900

    Time: 36 ms (60.34%), Space: 16.5 MB (83.99%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
@@ -1,12 +1,23 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        short_str = min(str1, str2)
-        a = ''
-        for i in range(len(short_str)):
-            ans = short_str[:i+1]
-            if str1.replace(ans, '') != '' or str2.replace(ans, '') != '':
-                ans == ''
-            else:
-                a = ans[:]
-        return a if a != '' else ''
+        # short_str = min(str1, str2)
+        # a = ''
+        # for i in range(len(short_str)):
+        #     ans = short_str[:i+1]
+        #     if str1.replace(ans, '') != '' or str2.replace(ans, '') != '':
+        #         ans == ''
+        #     else:
+        #         a = ans[:]
+        # return a if a != '' else ''
                 
+        short_str = min(str1, str2)
+
+        for i in range(len(short_str) , 0 , -1):
+            if len(str1) % i or len(str2) % i :
+                continue
+            n1 = len(str1) // i
+            n2 = len(str2) // i
+            base = str1[:i]
+            if str1 == n1 * base and str2 == n2*base:
+                return str1[:i]
+        return ''
