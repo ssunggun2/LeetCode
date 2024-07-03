90e762db4b221a7a12b9bb8bdf13ba6e6ceb535d
Sung Geun An <85274662+ssunggun2@users.noreply.github.com>
2024-07-03 16:14:36 +0900

    Time: 634 ms (5.04%), Space: 17 MB (21.74%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,28 +1,5 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        # def place(flowerbed, n, possible_place):
-        #     non_possible_place = []
-        #     plant_place = list(filter(lambda x: flowerbed[x] == 1, range(len(flowerbed))))
-        #     for i in plant_place:
-        #         non_possible_place.append(i)
-        #         if i - 1 >= 0:
-        #             non_possible_place.append(i - 1)
-        #         if i + 1 < len(flowerbed):
-        #             non_possible_place.append(i + 1)
-        #     target = list(set(possible_place) - set(non_possible_place))
-        #     if target == []:
-        #         return None, -1
-        #     flowerbed[target[0]] = 1
-        #     n -= 1
-        #     return flowerbed, n
-        # possible_place = [i for i in range(len(flowerbed))]
-        # for i in range(n):
-        #     flowerbed, n = place(flowerbed, n, possible_place)
-        #     if n == -1:
-        #         break
-
-        # return True if n == 0 else False
-
         def placing(flowerbed, n):
             if flowerbed[:2] == [1, 0]:
                 flowerbed = flowerbed[2:]
