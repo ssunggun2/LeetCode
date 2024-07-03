cbe15760ac578f872570e2e2692cb730cfb5f660
Sung Geun An <85274662+ssunggun2@users.noreply.github.com>
2024-07-03 16:25:50 +0900

    Time: 136 ms (31.95%), Space: 16.8 MB (84.87%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,27 +1,41 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        def placing(flowerbed, n):
-            if flowerbed[:2] == [1, 0]:
-                flowerbed = flowerbed[2:]
-            elif flowerbed[:3] == [0, 1, 0]:
-                flowerbed = flowerbed[3:]
-            elif flowerbed[:2] == [0, 0]:
-                flowerbed = flowerbed[2:]
-                n -= 1
-            elif flowerbed == [0]:
-                flowerbed = []
-                n -= 1
-            else:
-                n = -1
-            return flowerbed, n
-        
-        if n == 0:
-            return True
-
+        count = 0
         for i in range(len(flowerbed)):
-            flowerbed, n = placing(flowerbed, n)
-            if n == 0:
-                return True
-            elif n == -1:
-                break
-        return False
+            if  flowerbed[i] == 0 :
+                empty_left = (i == 0) or (flowerbed[i - 1] == 0)
+                empty_right = (i == len(flowerbed) -1) or (flowerbed[i + 1] == 0)
+                if empty_left and empty_right:
+                    flowerbed[i] = 1
+                    count += 1
+                    if count > n:
+                        break
+        return count >= n
+
+
+
+        # def placing(flowerbed, n):
+        #     if flowerbed[:2] == [1, 0]:
+        #         flowerbed = flowerbed[2:]
+        #     elif flowerbed[:3] == [0, 1, 0]:
+        #         flowerbed = flowerbed[3:]
+        #     elif flowerbed[:2] == [0, 0]:
+        #         flowerbed = flowerbed[2:]
+        #         n -= 1
+        #     elif flowerbed == [0]:
+        #         flowerbed = []
+        #         n -= 1
+        #     else:
+        #         n = -1
+        #     return flowerbed, n
+        
+        # if n == 0:
+        #     return True
+
+        # for i in range(len(flowerbed)):
+        #     flowerbed, n = placing(flowerbed, n)
+        #     if n == 0:
+        #         return True
+        #     elif n == -1:
+        #         break
+        # return False
