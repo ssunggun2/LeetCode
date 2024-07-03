f55bce924d2242db853b1005da78aa6c05519a0d
Sung Geun An <85274662+ssunggun2@users.noreply.github.com>
2024-07-03 14:27:12 +0900

    Time: 285 ms (32.08%), Space: 27.4 MB (39.86%) - LeetHub

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
@@ -1,18 +1,18 @@
-from typing import List
-
 class Solution:
     def minDifference(self, nums: List[int]) -> int:
         n = len(nums)
         if n <= 4:
             return 0
-        
         nums.sort()
+        ans = []
+
+        for i in range(n):
+            if n - 3 + i > n:
+                break
+            ans.append(nums[i: n - 3 + i ])
+        a = []
+        for i in ans:
+            a.append(i[-1] - i[0])
+        return min(a)
+
         
-        # 네 가지 경우의 차이를 계산
-        case1 = nums[-1] - nums[3]      # 처음 세 개 제거
-        case2 = nums[-2] - nums[2]      # 처음 두 개와 마지막 하나 제거
-        case3 = nums[-3] - nums[1]      # 처음 하나와 마지막 두 개 제거
-        case4 = nums[-4] - nums[0]      # 마지막 세 개 제거
-        
-        # 최소 차이를 반환
-        return min(case1, case2, case3, case4)
