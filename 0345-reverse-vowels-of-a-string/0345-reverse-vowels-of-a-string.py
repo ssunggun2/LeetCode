fa165a0c29ed0e17ccadd3cf48632be8f28227c5
Sung Geun An <85274662+ssunggun2@users.noreply.github.com>
2024-07-03 17:46:03 +0900

    Time: 54 ms (44.96%), Space: 18.7 MB (15.38%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -4,17 +4,8 @@ class Solution:
         for i, char in enumerate(s):
             if char in ["a" ,"e", "i", "o", "u" ,"A", "E", "I", "O","U"]:
                 char_loc.append(i)
-        # print(char_loc)
         s = list(s)
-        # print(s)
         for i in range(len(char_loc) // 2):
-            # print(i)
-            # print(char_loc[i])
-            # print(char_loc[-i - 1])
-            # print(s[char_loc[i]])
-            # print(s[char_loc[-i - 1]])
             s[char_loc[i]] , s[char_loc[-i - 1]] = s[char_loc[-i - 1]] , s[char_loc[i]]
-            # print(s)
         s =''.join(s)
-        # print(s)
         return s
