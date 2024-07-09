47361f4159167463fe17c6496d24d4a51ff61d8b
Sung Geun An <85274662+ssunggun2@users.noreply.github.com>
2024-07-09 17:16:33 +0900

    Time: 766 ms (28.52%), Space: 57.1 MB (45.36%) - LeetHub

--- 1803-average-waiting-time/1803-average-waiting-time.py
+++ 1803-average-waiting-time/1803-average-waiting-time.py
@@ -1,15 +1,11 @@
 class Solution:
     def averageWaitingTime(self, customers: List[List[int]]) -> float:
-        num_customers = len(customers)
         total_wait = 0
         end_time = customers[0][0]
-        for i in range(num_customers):
-            start_time = customers[i][0]
-            duration = customers[i][1]
+        for start_time, duration in customers:
             if end_time >= start_time:
                 end_time += duration
-                total_wait += end_time - start_time
             else:
                 end_time = start_time + duration
-                total_wait += duration
-        return total_wait / num_customers
+            total_wait += end_time - start_time
+        return total_wait / len(customers)
