63cd441b23f2cfa8a63443410be56c41729dce59
Sung Geun An <85274662+ssunggun2@users.noreply.github.com>
2024-07-12 00:35:55 +0900

    Time: 35 ms (61.58%), Space: 16.6 MB (40.89%) - LeetHub

--- 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.py
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.py
@@ -1,25 +1,3 @@
-# class Solution:
-#     def reverseParentheses(self, s: str) -> str:
-#         stack = []
-        
-#         for char in s:
-#             if char == ')':
-#                 # Pop characters until finding the opening parenthesis '('
-#                 temp = []
-#                 while stack and stack[-1] != '(':
-#                     temp.append(stack.pop())
-#                 # Pop the opening parenthesis '('
-#                 if stack:
-#                     stack.pop()
-#                 # Reverse the substring and push it back to stack
-#                 stack.extend(temp)
-#             else:
-#                 # Push current character to stack
-#                 stack.append(char)
-        
-#         # Join all characters to form the final result
-#         return ''.join(stack)
-
 class Solution:
     def reverseParentheses(self, s: str) -> str:
         stack = []
