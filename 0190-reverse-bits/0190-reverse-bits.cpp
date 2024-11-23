d027b9bd8c490bdd61a4b246e2c5e12080bec71a
Sung Geun An <85274662+ssunggun2@users.noreply.github.com>
2024-11-23 16:59:13 +0900

    Time: 0 ms (100%), Space: 7.4 MB (63.43%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,14 @@
+#include <bitset>
+
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t result = 0;
+        for (int i = 0; i < 32 ; i++) {
+            uint32_t bit = n & 1;
+            result = (result << 1) | bit;
+            n >>= 1;
+        }
+    return result;
+    }
+};
